3d41d2e8e537b3679832393eda5b31df28a9bc63
Paul Michali <pc@michali.net>
2015-12-04 19:32:45 +0000

    Adding callback mechanism
    Adding the callback mechanism to neutron-lib. Added code, test, and
    devref. Added additional test coverage. Added missing __init__.py
    files so tests are run. Needed oslo.log added to requirements.
    
    Once this is upstreamed, we can modify the callback mechanism.
    
    Change-Id: Ib16f3942e8ac2ddbfc8ff6919863ec9ad197e5b6
    Implements: blueprint neutron-lib

---
+++ neutron_lib/tests/unit/callbacks/test_callback_exceptions.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+test_callback_exceptions
+----------------------------------
+
+Tests for `neutron_lib.callback.exceptions` module.
+"""
+
+import functools
+
+import neutron_lib._callbacks.exceptions as ex
+from neutron_lib.tests import test_exceptions as base
+
+
+class TestCallbackExceptions(base.TestExceptions):
+
+    def _check_exception(self, exc_class, expected_msg, **kwargs):
+        raise_exc_class = functools.partial(base._raise, exc_class)
+        e = self.assertRaises(exc_class, raise_exc_class, **kwargs)
+        self.assertEqual(expected_msg, str(e))
+
+    def test_invalid(self):
+        self._check_exception(
+            ex.Invalid,
+            "The value 'foo' for bar is not valid.",
+            value='foo', element='bar')
+
+    def test_callback_failure(self):
+        self._check_exception(
+            ex.CallbackFailure,
+            'one',
+            errors='one')
+
+    def test_callback_failure_with_list(self):
+        self._check_exception(
+            ex.CallbackFailure,
+            '1,2,3',
+            errors=[1, 2, 3])
+
+    def test_notification_error(self):
+        '''Test that correct message is create for this error class.'''
+        error = ex.NotificationError('abc', 'boom')
+        self.assertEqual('Callback abc failed with "boom"', str(error))
