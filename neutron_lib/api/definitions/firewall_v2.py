645479ce1c2365e6397ac86f80253119186fe5c3
YAMAMOTO Takashi <yamamoto@midokura.com>
2017-02-02 13:36:00 +0900

    api: Fix firewall aliases
    Also, add fwaasrouterinsertion to KNOWN_EXTENSIONS.
    
    Change-Id: I06cba52c487e60bd5538e8de860576463381860e

--- neutron_lib/api/definitions/firewall_v2.py
+++ neutron_lib/api/definitions/firewall_v2.py
@@ -16,7 +16,7 @@ from neutron_lib import constants
 from neutron_lib.db import constants as db_const
 
 # The alias of the extension.
-ALIAS = 'firewall_v2'
+ALIAS = 'fwaas_v2'
 
 # Whether or not this extension is simply signaling behavior to the user
 # or it actively modifies the attribute map.
