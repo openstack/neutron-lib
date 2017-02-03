534333ca9cc3bc7dec91c3f4f9928e2c80149016
Boden R <bodenvmw@gmail.com>
2017-02-03 09:31:28 -0700

    rehome NeutronWorker class
    This patch rehomes neutron.worker.NeutronWorker as
    neutron_lib.worker.BaseWorker. While this class is only
    used by networking-ovn out of the neutron tree today [1],
    the ML2 MechanismDriver (publically extendable) supports
    workers directly [2] (ex: get_workers()). As a result the worker
    is part of our publicly extendable API and thus seems a likely
    candidate for neutron-lib.
    
    UTs and a release note are also included.
    
    [1] http://codesearch.openstack.org/?q=NeutronWorker
    [2] https://github.com/openstack/neutron/blob/master/neutron/plugins/ml2/driver_api.py#L981
    
    Change-Id: Id1be78e483da7474e9007c7180e92fbf46863a52

---
+++ neutron_lib/worker.py
@@ -0,0 +1,72 @@
+# All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_service import service
+
+from neutron_lib.callbacks import events
+from neutron_lib.callbacks import registry
+from neutron_lib.callbacks import resources
+
+
+class BaseWorker(service.ServiceBase):
+    """Partial implementation of the ServiceBase ABC.
+
+    Subclasses will still need to add the other abstract methods defined in
+    service.ServiceBase. See oslo_service for more details.
+
+    If a plugin needs to handle synchronization with the Neutron database and
+    do this only once instead of in every API worker, for instance, it would
+    define a BaseWorker class and the plugin would have get_workers return
+    an array of BaseWorker instances. For example:
+        class MyPlugin(...):
+            def get_workers(self):
+                return [MyPluginWorker()]
+
+        class MyPluginWorker(BaseWorker):
+            def start(self):
+                super(MyPluginWorker, self).start()
+                do_sync()
+    """
+
+    # default class value for case when super().__init__ is not called
+    _default_process_count = 1
+
+    def __init__(self, worker_process_count=_default_process_count):
+        """Initialize a worker instance.
+
+        :param worker_process_count: Defines how many processes to spawn for
+            worker:
+                0 - spawn 1 new worker thread,
+                1..N - spawn N new worker processes
+        """
+        self._worker_process_count = worker_process_count
+
+    @property
+    def worker_process_count(self):
+        """The worker's process count.
+
+        :returns: The number of processes to spawn for this worker.
+        """
+        return self._worker_process_count
+
+    def start(self):
+        """Start the worker.
+
+        If worker_process_count is greater than 0, a callback notification
+        is sent. Subclasses should call this method before doing their
+        own start() work.
+        :returns: None
+        """
+        if self.worker_process_count > 0:
+            registry.notify(resources.PROCESS, events.AFTER_INIT, self.start)
