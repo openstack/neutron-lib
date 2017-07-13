13f3ea617b1ba7544a3aa1ec394bb31345b2394a
Hirofumi Ichihara <ichihara.hirofumi@lab.ntt.co.jp>
2017-07-13 14:28:36 +0900

    Remove unnecessary variables in UT
    In unit tests of API definition, there are extension_resources which
    defines core resources although they are not resources of extension.
    This patch removes them.
    
    TrivialFix
    
    Change-Id: I13ebffa80d5d5910840093de092d3bbe1a93a9c4

--- neutron_lib/tests/unit/api/definitions/test_dns_domain_ports.py
+++ neutron_lib/tests/unit/api/definitions/test_dns_domain_ports.py
@@ -16,11 +16,9 @@
 
 from neutron_lib.api.definitions import dns
 from neutron_lib.api.definitions import dns_domain_ports
-from neutron_lib.api.definitions import port
 from neutron_lib.tests.unit.api.definitions import base
 
 
 class DnsDomainPortsDefinitionTestCase(base.DefinitionBaseTestCase):
     extension_module = dns_domain_ports
-    extension_resources = (port.COLLECTION_NAME,)
     extension_attributes = (dns.DNSDOMAIN,)
