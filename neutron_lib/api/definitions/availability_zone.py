c9f42f3fa46745aee063cd7903d372d45ee8cbb1
Boden R <bodenvmw@gmail.com>
2017-08-04 10:51:43 -0600

    rehome availability zone API def
    This patch rehomes neutron's availability zone extension into
    neutron-libs API definition layout/structure. UTs and a reno are also
    included.
    
    Change-Id: Id8513a7a82609808829e3e66827f69f3fd0c3e4d

---
+++ neutron_lib/api/definitions/availability_zone.py
@@ -0,0 +1,49 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from neutron_lib.api.definitions import agent
+from neutron_lib.api import validators
+from neutron_lib.api.validators import availability_zone as az_validator
+
+
+validators.add_validator('availability_zone_hint_list',
+                         az_validator._validate_availability_zone_hints)
+
+AZ_HINTS = 'availability_zone_hints'
+
+ALIAS = 'availability_zone'
+IS_SHIM_EXTENSION = False
+IS_STANDARD_ATTR_EXTENSION = False
+NAME = 'Availability Zone'
+API_PREFIX = ''
+DESCRIPTION = 'The availability zone extension.'
+UPDATED_TIMESTAMP = '2015-01-01T10:00:00-00:00'
+RESOURCE_NAME = 'availability_zone'
+COLLECTION_NAME = RESOURCE_NAME + 's'
+RESOURCE_ATTRIBUTE_MAP = {
+    COLLECTION_NAME: {
+        'name': {'is_visible': True},
+        'resource': {'is_visible': True},
+        'state': {'is_visible': True}
+    },
+    agent.COLLECTION_NAME: {
+        RESOURCE_NAME: {
+            'allow_post': False, 'allow_put': False,
+            'is_visible': True}
+    }
+}
+SUB_RESOURCE_ATTRIBUTE_MAP = {}
+ACTION_MAP = {}
+REQUIRED_EXTENSIONS = [agent.ALIAS]
+OPTIONAL_EXTENSIONS = []
+ACTION_STATUS = {}
---
+++ neutron_lib/tests/unit/api/validators/test_availability_zone.py
@@ -0,0 +1,39 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from neutron_lib.api.validators import availability_zone as az_validator
+from neutron_lib.db import constants as db_const
+from neutron_lib import exceptions
+from neutron_lib.tests import _base as base
+
+
+class TestAvailabilityZoneValidator(base.BaseTestCase):
+
+    @mock.patch.object(az_validator.validators,
+                       'validate_list_of_unique_strings',
+                       return_value='bad')
+    def test__validate_availability_zone_hints_unique_strings(
+            self, mock_unique_strs):
+        self.assertEqual(
+            'bad', az_validator._validate_availability_zone_hints(['a', 'a']))
+
+    def test__validate_availability_zone_hints_excessive_len(self):
+        self.assertRaisesRegex(
+            exceptions.InvalidInput, 'Too many availability_zone_hints',
+            az_validator._validate_availability_zone_hints,
+            ['a' * (db_const.AZ_HINTS_DB_LEN + 1)])
+
+    def test__validate_availability_zone_hints_valid_input(self):
+        self.assertIsNone(
+            az_validator._validate_availability_zone_hints(['a', 'b', 'c']))
