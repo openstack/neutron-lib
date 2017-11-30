f38df80ca5be7e828de897ba7d5421ce3377476f
Boden R <bodenvmw@gmail.com>
2017-11-30 13:15:14 -0700

    rehome revisionifmatch api def
    This patch rehomes the revisionifmatch extension's API definition into
    neutron-lib.
    
    Change-Id: I46e30079bc0708b7f55d404c262226cf986e5b66

---
+++ neutron_lib/api/definitions/revisionifmatch.py
@@ -0,0 +1,27 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ALIAS = 'revision-if-match'
+IS_SHIM_EXTENSION = True
+IS_STANDARD_ATTR_EXTENSION = False
+NAME = 'If-Match constraints based on revision_number'
+API_PREFIX = ''
+DESCRIPTION = ("Extension indicating that If-Match based on revision_number "
+               "is supported.")
+UPDATED_TIMESTAMP = '2016-12-11T00:00:00-00:00'
+RESOURCE_ATTRIBUTE_MAP = {}
+SUB_RESOURCE_ATTRIBUTE_MAP = {}
+ACTION_MAP = {}
+REQUIRED_EXTENSIONS = ['standard-attr-revisions']
+OPTIONAL_EXTENSIONS = []
+ACTION_STATUS = {}
