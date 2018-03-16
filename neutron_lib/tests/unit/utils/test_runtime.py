6f94faf64ee2fe48457ad799172555cc84c2812f
Boden R <bodenvmw@gmail.com>
2018-03-16 10:04:02 -0600

    versioned object plugin registry
    This patch implements the plumbing to support dynamically loadable
    plugins for neutron versioned objects as per the spec [1]. Specifically
    a utility class is introduced as a generic plugin manager for stevedore
    based plugins in a given namespace. A global singleton instance of this
    plugin manager is then wrapped to provide access to neutron versioned
    objects.
    
    Using this implementation:
    - Neutron version object classes are registered as entry points. For
    example [2].
    - Consumers can then use the object registry in neutron_lib to access
    them, for example [3].
    
    As-is this change doesn't introduce any breakage risk; it's new
    functionality that no one is using.
    
    [1] I079d06502e6e7b1e20aea882979b0ecd9106eaeb
    [2] https://review.openstack.org/#/c/553836/
    [3] https://review.openstack.org/#/c/553835/
    
    Change-Id: I39d9bab1e24fbcbd5f9b3abf80560da920f1cf26

--- neutron_lib/tests/unit/utils/test_runtime.py
+++ neutron_lib/tests/unit/utils/test_runtime.py
@@ -12,6 +12,7 @@
 # limitations under the License.
 
 import mock
+from stevedore import enabled
 
 from neutron_lib.tests import _base as base
 from neutron_lib.utils import runtime
@@ -51,3 +52,61 @@ class TestRunTime(base.BaseTestCase):
         self.assertEqual(
             mock.sentinel.dummy_class,
             runtime.load_class_by_alias_or_classname('ns', 'n'))
+
+
+class TestNamespacedPlugins(base.BaseTestCase):
+
+    @mock.patch.object(enabled, 'EnabledExtensionManager')
+    def test_init_reload(self, mock_mgr):
+        plugins = runtime.NamespacedPlugins('_test_ns_')
+        mock_mgr.assert_called_with(
+            '_test_ns_', mock.ANY, invoke_on_load=False)
+        mock_mgr().map.assert_called_with(plugins._add_extension)
+
+    @mock.patch.object(runtime, 'LOG')
+    @mock.patch.object(enabled, 'EnabledExtensionManager')
+    def test_init_reload_no_plugins(self, mock_mgr, mock_log):
+        mock_mgr().names.return_value = []
+        plugins = runtime.NamespacedPlugins('_test_ns_')
+        mock_log.debug.assert_called_once()
+        mock_mgr().map.assert_not_called()
+        self.assertDictEqual({}, plugins._extensions)
+
+    @mock.patch.object(enabled, 'EnabledExtensionManager')
+    def test_add_duplicate_names(self, mock_mgr):
+        mock_ep = mock.Mock()
+        mock_ep.name = 'a'
+        mock_mgr().names.return_value = ['a', 'a']
+        # return 2 EPs with the same name
+        mock_mgr().map = lambda f: [f(ep) for ep in [mock_ep, mock_ep]]
+        self.assertRaises(KeyError, runtime.NamespacedPlugins, '_test_ns_')
+
+    @mock.patch.object(enabled, 'EnabledExtensionManager')
+    def test_get_plugin_class(self, mock_mgr):
+        mock_epa = mock.Mock()
+        mock_epa.name = 'a'
+        mock_epa.plugin = 'A'
+        mock_epb = mock.Mock()
+        mock_epb.name = 'b'
+        mock_epb.plugin = 'B'
+        mock_mgr().names.return_value = ['a', 'b']
+        mock_mgr().map = lambda f: [f(ep) for ep in [mock_epa, mock_epb]]
+
+        plugins = runtime.NamespacedPlugins('_test_ns_')
+        self.assertEqual('A', plugins.get_plugin_class('a'))
+        self.assertEqual('B', plugins.get_plugin_class('b'))
+
+    @mock.patch.object(enabled, 'EnabledExtensionManager')
+    def test_new_plugin_instance(self, mock_mgr):
+        mock_epa = mock.Mock()
+        mock_epa.name = 'a'
+        mock_epb = mock.Mock()
+        mock_epb.name = 'b'
+        mock_mgr().names.return_value = ['a', 'b']
+        mock_mgr().map = lambda f: [f(ep) for ep in [mock_epa, mock_epb]]
+
+        plugins = runtime.NamespacedPlugins('_test_ns_')
+        plugins.new_plugin_instance('a', 'c', 'd', karg='kval')
+        plugins.new_plugin_instance('b')
+        mock_epa.plugin.assert_called_once_with('c', 'd', karg='kval')
+        mock_epb.plugin.assert_called_once_with()
