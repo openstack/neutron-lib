2dc9675f0f1cc55f988dfa4ff78a459785ce6b2f
Boden R <bodenvmw@gmail.com>
2018-04-03 11:30:56 -0600

    rehome qos bw limit direction api def
    This patch rehomes the qos bandwidth limit direction extension's API
    definition into neutron-lib. This definition extends an existing
    sub-resource and thus requires the unit test changes in commit
    Ib91004490dc3ca71b7caafe5cc3cb07417be1e4c
    
    For sample consumption see commit
    I4cea734dd713f00d6e4c416a50c4dde620dc644d
    
    Change-Id: I823f567ddf9c895eb3cf94e95ae34a382d67f51d

---
+++ neutron_lib/tests/unit/api/definitions/test_qos_bw_limit_direction.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2017 OVH SAS
+# All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron_lib.api.definitions import qos
+from neutron_lib.api.definitions import qos_bw_limit_direction
+from neutron_lib.tests.unit.api.definitions import base
+
+
+class QoSBwLimitDirectionDefinitionTestCase(base.DefinitionBaseTestCase):
+    extension_module = qos_bw_limit_direction
+    extension_subresources = (qos.BANDWIDTH_LIMIT_RULES,)
+    extension_attributes = (qos_bw_limit_direction.DIRECTION,)
