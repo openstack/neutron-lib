355864d37666a9f5c3709ae46b9e73ff36d39fcf
Boden R <bodenvmw@gmail.com>
2019-02-13 12:51:03 -0700

    rehome remaining common constants
    This patch rehomes the remaining neutron.common.constants into
    neutron-lib. All rehomed constants are put into neutron_lib.constants
    except for EXT_PARENT_RESOURCE_MAPPING which lives in
    neutron_lib.services.constants due to circular import refs when rehoming
    it into neutron_lib.constants or neutron_lib.plugin.constants.
    
    Change-Id: Iac637a413d398f69b1199dfe2ad16b327387a032

---
+++ neutron_lib/services/constants.py
@@ -0,0 +1,29 @@
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron_lib.api.definitions import l3
+from neutron_lib.plugins import constants as plugin_const
+
+
+# Registered extension parent resource check mapping
+# If we want to register some service plugin resources into policy and check
+# the owner when operating their subresources. We can write here to use
+# existing policy engine for parent resource owner check.
+# Each entry here should be PARENT_RESOURCE_NAME: SERVICE_PLUGIN_NAME,
+# PARENT_RESOURCE_NAME is usually from api definition.
+# SERVICE_PLUGIN_NAME is the service plugin which introduced the resource and
+# registered the service plugin name in neutron-lib.
+EXT_PARENT_RESOURCE_MAPPING = {
+    l3.FLOATINGIP: plugin_const.L3
+}
