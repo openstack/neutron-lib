3fd0a3c4d7d54ba438746365653a9d379c842f08
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2019-04-09 09:35:54 +0000

    Rehome provider_network_attribute_updates_supported to API definition
    Rehomed provider_network_attribute_updates_supported to
    plugins.ml2.api.MechanismDriver, in order to be inherited in other
    mechanism drivers.
    
    Change-Id: I145861b0ca356e3e27495fb2b0205f6e8339a964
    Closes-Bug: #1823865

--- neutron_lib/plugins/ml2/api.py
+++ neutron_lib/plugins/ml2/api.py
@@ -447,6 +447,17 @@ class MechanismDriver(object):
         """
         return False
 
+    @staticmethod
+    def provider_network_attribute_updates_supported():
+        """Returns the provider network attributes that can be updated
+
+        Possible values: neutron_lib.api.definitions.provider_net.ATTRIBUTES
+
+        :returns: (list) provider network attributes that can be updated in a
+                         live network using this driver.
+        """
+        return []
+
 
 @six.add_metaclass(abc.ABCMeta)
 class _TypeDriverBase(object):
