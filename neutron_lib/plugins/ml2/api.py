b4e33e5e7aba062027765ed56912ad8da4ca8351
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2020-07-27 18:06:35 +0000

    Add "supported_extensions" method to MechanismDriver
    This method returns the supported plugin extensions, given a
    set of them. Each mechanism driver will be able to implement each
    own filtering method.
    
    By default, the method will return the same set of extensions
    without any filtering.
    
    Related-Bug: #1888829
    
    Change-Id: I6fd62f6f393b635e6710020adab736a09a142556

--- neutron_lib/plugins/ml2/api.py
+++ neutron_lib/plugins/ml2/api.py
@@ -455,6 +455,21 @@ class MechanismDriver(object, metaclass=abc.ABCMeta):
         """
         return []
 
+    def supported_extensions(self, extensions):
+        """Return the mechanism driver supported extensions
+
+        By default this method will return the same provided set, without any
+        filtering. In case any particular mechanism driver needs to filter out
+        any specific extension or supports only a reduced set of extensions,
+        this method should be override.
+
+        :param extensions: set of extensions supported by the instance that
+                           created this mechanism driver.
+        :returns: a set of the extensions currently supported by this
+                  mechanism driver
+        """
+        return extensions
+
 
 class _TypeDriverBase(object, metaclass=abc.ABCMeta):
 
