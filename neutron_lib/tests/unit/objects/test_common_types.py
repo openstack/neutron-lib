5ef0d26ad372031c4118304f29bebb963a7981f4
Brian Haley <haleyb.dev@gmail.com>
2022-03-15 20:34:25 -0400

    Add NumaAffinityPoliciesEnumField class unit tests
    Prior change rehomed this class here, add some basic
    unit tests to cover it.
    
    Change-Id: Ide8229f101456bc448f4acc78ff74b879809261a
    Related-Bug: #1886798

--- neutron_lib/tests/unit/objects/test_common_types.py
+++ neutron_lib/tests/unit/objects/test_common_types.py
@@ -329,3 +329,20 @@ class NetworkSegmentRangeNetworkTypeEnumFieldTest(test_base.BaseTestCase,
     def test_stringify(self):
         for in_val, out_val in self.coerce_good_values:
             self.assertEqual("'%s'" % in_val, self.field.stringify(in_val))
+
+
+class NumaAffinityPoliciesEnumFieldTest(test_base.BaseTestCase, TestField):
+    def setUp(self):
+        super().setUp()
+        self.field = common_types.NumaAffinityPoliciesEnumField()
+        good_values = [const.PORT_NUMA_POLICY_REQUIRED,
+                       const.PORT_NUMA_POLICY_PREFERRED,
+                       const.PORT_NUMA_POLICY_LEGACY]
+        self.coerce_good_values = [(val, val) for val in good_values]
+        self.coerce_bad_values = ['foo-invalid-policy']
+        self.to_primitive_values = self.coerce_good_values
+        self.from_primitive_values = self.coerce_good_values
+
+    def test_stringify(self):
+        for in_val, out_val in self.coerce_good_values:
+            self.assertEqual("'%s'" % in_val, self.field.stringify(in_val))
