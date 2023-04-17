872807640227193bf3bfb4800b2253ea975538e9
liushy <liuxie_11@163.com>
2023-04-17 16:55:07 +0800

    New api-def: allowedaddresspairs-atomic
    Partial-Bug: #2012332
    Change-Id: Ibc713217ffb67ce35e03ae7f341a302a9edef4d1

--- neutron_lib/api/definitions/base.py
+++ neutron_lib/api/definitions/base.py
@@ -82,6 +82,7 @@ KNOWN_EXTENSIONS = (
     'agent',
     'agent-resources-synced',
     'allowed-address-pairs',
+    'allowed-address-pairs-atomic',
     'auto-allocated-topology',
     'availability_zone',
     'binding',
