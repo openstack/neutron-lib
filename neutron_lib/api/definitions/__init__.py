b7598dab47d3648bf1bb28eae8c8ed8006097f5a
Bodo Petermann <b.petermann@syseleven.de>
2023-10-18 13:50:58 +0200

    vpnaas: add support for more ciphers (auth, encryption, pfs modes)
    Encryption algorithms: add AES CCM mode and AES GCM mode variants
    for 128/192/256 bit keys and 8/12/16 octet ICVs.
    Auth algorithms: add aes-xcbc and aes-cmac.
    PFS: add Diffie Hellman groups 15 to 31.
    
    Related-Bug: #1938284
    Change-Id: Iba86fe9a1bbf88223b57a45fb89349c6b1858015

--- neutron_lib/api/definitions/__init__.py
+++ neutron_lib/api/definitions/__init__.py
@@ -159,6 +159,7 @@ from neutron_lib.api.definitions import uplink_status_propagation
 from neutron_lib.api.definitions import vlan_filter
 from neutron_lib.api.definitions import vlantransparent
 from neutron_lib.api.definitions import vpn
+from neutron_lib.api.definitions import vpn_aes_ccm_gcm
 from neutron_lib.api.definitions import vpn_endpoint_groups
 from neutron_lib.api.definitions import vpn_flavors
 
@@ -312,6 +313,7 @@ _ALL_API_DEFINITIONS = {
     vlan_filter,
     vlantransparent,
     vpn,
+    vpn_aes_ccm_gcm,
     vpn_endpoint_groups,
     vpn_flavors,
 }
