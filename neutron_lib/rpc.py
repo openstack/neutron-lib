23b1fbc3b37f2b53061500b2a5034cf5d074abad
Ihar Hrachyshka <ihrachys@redhat.com>
2024-02-14 17:34:04 +0000

    rpc: fix some typos
    Change-Id: I3669e11839a99e7d1f60b5be16f1097295196628

--- neutron_lib/rpc.py
+++ neutron_lib/rpc.py
@@ -239,9 +239,9 @@ def get_notifier(service=None, host=None, publisher_id=None):
     """Get a new notifier reference.
 
     :param service: The optional service for the notifier.
-    :param host: The optional host for the notifier. If not given the host
+    :param host: The optional host for the notifier. If not given, the host
         will be taken from the global CONF.
-    :param publisher_id: The optional publisher ID for the notifer.
+    :param publisher_id: The optional publisher ID for the notifier.
     :returns: A new RPC notifier reference.
     """
     if NOTIFIER is None:
