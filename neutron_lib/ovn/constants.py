454afda941233ee46bf77be858b67f87c2bdf1d8
Slawek Kaplonski <skaplons@redhat.com>
2026-01-19 14:37:48 +0100

    Rehome ovn-db-sync-util related constants to neutron-lib
    Some constants which were defined in the neutron.common.ovn.constants
    module so far may now be used by other stadium projects, like e.g.
    neutron-fwaas so it would be better to have them in neutron-lib instead
    of keeping them in neutron repo.
    This patch rehomes such constants to the new module
    `neutron_lib.ovn.constants`.
    
    Related-bug: #2130570
    
    Change-Id: Iffecb7b3ab0c28a6256e5854762b9a24c0097da8
    Signed-off-by: Slawek Kaplonski <skaplons@redhat.com>

---
+++ neutron_lib/ovn/constants.py
@@ -0,0 +1,17 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# OVN db sync mode options
+OVN_DB_SYNC_MODE_OFF = 'off'
+OVN_DB_SYNC_MODE_LOG = 'log'
+OVN_DB_SYNC_MODE_REPAIR = 'repair'
+OVN_DB_SYNC_MODE_MIGRATE = 'migrate'
