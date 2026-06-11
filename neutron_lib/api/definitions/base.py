675d19034feb91d8edd82559a8ebbfee2c047f0e
Brian Haley <haleyb.dev@gmail.com>
2026-06-11 16:17:26 -0400

    Revert "New api-def: allowedaddresspairs-atomic"
    This reverts commit 872807640227193bf3bfb4800b2253ea975538e9.
    
    Reason for revert: neutron and client portions of this RFE
    were never completed, so removing the API ref.
    
    Change-Id: If0f0a155dd4c88299ab894a357e3195a83546ac6
    Signed-off-by: Brian Haley <haleyb.dev@gmail.com>

--- neutron_lib/api/definitions/base.py
+++ neutron_lib/api/definitions/base.py
@@ -83,7 +83,6 @@ KNOWN_EXTENSIONS = (
     'agent',
     'agent-resources-synced',
     'allowed-address-pairs',
-    'allowed-address-pairs-atomic',
     'auto-allocated-topology',
     'availability_zone',
     'binding',
